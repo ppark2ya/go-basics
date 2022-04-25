10b2f87955447267282971a5cbb4f3ae1ea15247
jtpark <vigabhan1231@gmail.com>
2022-04-25 22:56:05 +0900

    dictionary type method 추가: Update, Delete

--- main.go
+++ main.go
@@ -3,35 +3,22 @@ package main
 import (
 	"fmt"
 
-	"github.com/ppark2ya/go-basics/accounts"
 	"github.com/ppark2ya/go-basics/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("jtpark")
-	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		// Println을 실행하고 프로그램을 종료함
-		// log.Fatalln(err)
-		fmt.Println(err)
-	}
-	// fmt.Println(account.Balance(), account.Owner())
-	fmt.Println(account)
-
 	dictionary := mydict.Dictionary{"fruit": "apple"}
-	// dictionary["hello"] = "hello world"
-	// fmt.Println(dictionary)
-	isExists := dictionary.Add("fruit", "apple")
-	fmt.Println("isExists:", isExists)
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	// err := dictionary.Update(baseWord, "Second")
 
-	dictionary.Add("second", "banana")
-	definition, err := dictionary.Search("second")
-
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println("definition: ", definition)
 	}
+	fmt.Println(word)
 }
--- mydict/mydict.go
+++ mydict/mydict.go
@@ -6,8 +6,14 @@ import (
 
 type Dictionary map[string]string
 
-var errNotFound = errors.New("not found")
-var errWordExists = errors.New("that word already exists")
+// var errNotFound = errors.New("not found")
+// var errCantUpdate = errors.New("can't update non-existing word")
+// var errWordExists = errors.New("that word already exists")
+var (
+	errNotFound = errors.New("not found")
+	errCantUpdate = errors.New("can't update non-existing word")
+	errWordExists = errors.New("that word already exists")
+)
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error)  {
@@ -30,4 +36,22 @@ func (d Dictionary) Add(word, def string) error {
 		return errWordExists
 	}
 	return nil
+}
+
+// Update a word
+func (d Dictionary) Update(word, def string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
