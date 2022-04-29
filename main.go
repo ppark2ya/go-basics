01d67387ff4c23dcf637aeead5adc969357f2849
jtpark <vigabhan1231@gmail.com>
2022-04-29 18:54:52 +0900

    goroutines basic

--- main.go
+++ main.go
@@ -1,51 +1,20 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("Request failed")
-
 func main() {
-	// make empty map
-	// same: var results = map[string]string{}
-	var results = make(map[string]string)
-
-	urls := []string{
-		"https://www.google.com/",
-		"https://www.naver.com/",
-		"https://www.daum.net/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-	}
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitUrl(url)
-
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
+	// goroutines -> main 함수가 동작하는 동안 병렬로 실행
+	go sexyCount("nico")
+	go sexyCount("jtpark")
+	time.Sleep(time.Second * 5)
 }
 
-func hitUrl(url string) error {
-	fmt.Println("CHECKING:", url)
-	resp, err := http.Get(url)
-
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
 	}
-
-	return nil
 }
